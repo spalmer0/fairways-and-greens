0a0e1032f91261e3ab93cd9ad77be3836fdf8983
Stuart Palmer <palmersf@gmail.com>
2020-11-18 22:58:28 -0500

    adds initial login setup

--- server.js
+++ server.js
@@ -7,20 +7,34 @@ const indexRouter = require('./routes/index');
 const playersRouter = require('./routes/players');
 
 const morgan = require('morgan');
+const session = require('express-session');
+const passport = require('passport');
+
+// load the env vars
+require('dotenv').config()
 
 // create the express app
 const app = express();
 
 // configure server settings
 require('./config/database');
+require('./config/passport');
 
+// view engine setup
 app.set('view engine', 'ejs');
 
 // mount middleware
 app.use(morgan('dev'));
 app.use(methodOverride('_method'));
 app.use(express.static('public'));
-app.use(express.urlencoded({ extended: false}));
+app.use(express.urlencoded({ extended: true }));
+app.use(session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: true
+}));
+app.use(passport.initialize());
+app.use(passport.session());
 
 // mount routes
 app.use('/', indexRouter);
